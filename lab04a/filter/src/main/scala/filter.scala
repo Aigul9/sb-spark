a1b308661e6385f4e90f66f9be5ffcc058ab34d1
Aigul <46625461+Aigul9@users.noreply.github.com>
2023-10-27 00:14:22 +0300

    fix json

--- lab04a/filter/src/main/scala/filter.scala
+++ lab04a/filter/src/main/scala/filter.scala
@@ -9,6 +9,7 @@ object filter {
 
     def write(df: DataFrame, path: String): Unit = {
       df
+        .select("data.*", "p_date")
         .write
         .partitionBy("p_date")
         .option("quote", "")
@@ -62,7 +63,7 @@ object filter {
       .select(col("value").cast("string").alias("value_string"))
       .withColumn("value_data", from_json(col("value_string"), jsonSchema))
       .withColumn("p_date", from_unixtime(col("value_data.timestamp") / 1000, "yyyyMMdd"))
-      .selectExpr("to_json(struct(value_data.*, p_date as date)) as new_json", "p_date")
+      .selectExpr("struct(value_data.*, p_date as date) as data", "p_date")
 
     val view = res.filter(col("value_data.event_type") === "view")
     val buy = res.filter(col("value_data.event_type") === "buy")
